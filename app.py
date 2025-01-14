642bb87d853c7cb17df4a1be9f2472805e0b9de2
Bishal Budhathoki <35174206+BishalBudhathoki@users.noreply.github.com>
2025-01-14 22:23:18 +1100

    gunicorn introduced

--- app.py
+++ app.py
@@ -183,4 +183,4 @@ def preview(filename):
     return render_template('preview.html', filename=filename)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
