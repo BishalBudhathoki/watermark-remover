bea8df3408e7385c1941fa3310919e49f0afe588
Bishal Budhathoki <35174206+BishalBudhathoki@users.noreply.github.com>
2025-01-14 23:01:16 +1100

    minor changes

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,5 +1,7 @@
 # Gunicorn configuration file
-bind = "0.0.0.0:8000"
-workers = 4
-threads = 2
-timeout = 120 
+import os
+
+bind = f"0.0.0.0:{os.getenv('PORT', '8000')}"  # Default to 8000 if PORT is not set
+workers = 2  # Adjust based on your app's load
+threads = 2  # Optional: Use threads if necessary
+timeout = 30  # Optional: Set timeout to handle long requests
